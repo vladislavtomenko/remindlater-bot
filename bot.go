e3f9e93433f49508a62f77460e71a5bbe9c6b03d
Vladislav Tomenko <vladislavtomenko@gmail.com>
2018-05-05 02:57:34 +0300

    Edit bot reply on callback action (#6)
    * feat(bot.go): add IsTimeToProcess method to Reminder
    
    * fix(bot.go): snooze for  3 hours button
    
    * feat(bot.go): edit bot reply on button click
    
    * fix(bot.go): delete SendMesg func
    
    * chore(bot.go): rename Processor functions to Handler
    
    * chore(bot.go): add comments to code

--- bot.go
+++ bot.go
@@ -9,6 +9,7 @@ import (
 	"gopkg.in/telegram-bot-api.v4"
 )
 
+// Reminder is a sent user message and a date when remind about it.
 type Reminder struct {
 	Message *tgbotapi.Message
 
@@ -16,34 +17,34 @@ type Reminder struct {
 	RemindTime int
 }
 
+// Check if it's time to send the reminder.
+func (reminder Reminder) IsTimeToProcess() bool {
+	return reminder.RemindTime <= int(time.Now().Unix())
+}
+
 type ReminderResponse struct {
 	Reminder *Reminder
 	Message  *tgbotapi.Message
 }
 
+// Config is the bot configuration.
+//
+// Token is a telegram bot API token,
+// Duration is the default reminder delay in seconds ,
+// Debug enables extended output.
 type Config struct {
 	Token    string
 	Duration int
 	Debug    bool
 }
 
-func SendMessage(message tgbotapi.MessageConfig, bot *tgbotapi.BotAPI) (*tgbotapi.Message, error) {
+// CallbackHandler handles the callbacks from inline buttons.
+//
+// It gets the callback, updates (or remove) a reminder in reminders,
+// and send a message to the bot.
+func CallbackHandler(callback tgbotapi.CallbackQuery, reminders *[]*Reminder, bot *tgbotapi.BotAPI) {
 
-	sentMessage, err := bot.Send(message)
-	return &sentMessage, err
-
-}
-
-func CallbackProcessor(callback tgbotapi.CallbackQuery, reminders *[]*Reminder, bot *tgbotapi.BotAPI) {
-	edit := tgbotapi.NewEditMessageReplyMarkup(
-		callback.Message.Chat.ID,
-		callback.Message.MessageID,
-		tgbotapi.InlineKeyboardMarkup{
-			InlineKeyboard: make([][]tgbotapi.InlineKeyboardButton, 0),
-		},
-	)
-
-	bot.Send(edit)
+	var newReplyText string
 
 	var reminderIndex int
 	for i, reminder := range *reminders {
@@ -54,64 +55,95 @@ func CallbackProcessor(callback tgbotapi.CallbackQuery, reminders *[]*Reminder, 
 
 	if callback.Data == "Complete" {
 		*reminders = append((*reminders)[:reminderIndex], (*reminders)[reminderIndex+1:]...)
-		return
+		newReplyText = "Completed \u2705"
+	} else {
+		newRemindTime := int(time.Now().Unix())
+
+		switch callback.Data {
+		case "5 sec":
+			newRemindTime += 5
+		case "20 min":
+			newRemindTime += 1200
+		case "1 hour":
+			newRemindTime += 3600
+		case "3 hours":
+			newRemindTime += 10800
+		case "1 day":
+			newRemindTime += 86400
+		}
+
+		(*reminders)[reminderIndex].RemindTime = newRemindTime
+		(*reminders)[reminderIndex].IsSent = false
+
+		newReplyText = "Snoozed for " + callback.Data + " \u23F3"
 	}
 
-	newRemindTime := int(time.Now().Unix())
-	switch callback.Data {
-	case "sec5":
-		newRemindTime += 5
-	case "min20":
-		newRemindTime += 1200
-	case "hour1":
-		newRemindTime += 3600
-	case "hour3":
-		newRemindTime += 10800
-	case "day1":
-		newRemindTime += 86400
-	}
-
-	(*reminders)[reminderIndex].RemindTime = newRemindTime
-	(*reminders)[reminderIndex].IsSent = false
+	// Hide the inline buttons.
+	editMarkup := tgbotapi.NewEditMessageReplyMarkup(
+		callback.Message.Chat.ID,
+		callback.Message.MessageID,
+		tgbotapi.InlineKeyboardMarkup{
+			InlineKeyboard: make([][]tgbotapi.InlineKeyboardButton, 0),
+		},
+	)
+	// Edit the reply text.
+	editText := tgbotapi.NewEditMessageText(
+		callback.Message.Chat.ID,
+		callback.Message.MessageID,
+		newReplyText,
+	)
+	bot.Send(editMarkup)
+	bot.Send(editText)
 
 }
 
-func ReminderProcessor(reminders *[]*Reminder, bot *tgbotapi.BotAPI) {
+// ReminderHandler handles the reminders queue.
+//
+// The function sends a reminder if it's time to remind.
+// Otherwise it waits for an appropriate reminder.
+func ReminderHandler(reminders *[]*Reminder, bot *tgbotapi.BotAPI) {
 	for {
 		for i := len(*reminders); i > 0; i-- {
 			reminder := (*reminders)[i-1]
 
-			if (*reminder).RemindTime <= int(time.Now().Unix()) && (*reminder).IsSent == false {
+			if (*reminder).IsTimeToProcess() && (*reminder).IsSent == false {
 				msg := tgbotapi.NewMessage((*reminder).Message.Chat.ID, "You asked me to remind you about this message. Snooze it?")
 				msg.ReplyToMessageID = (*reminder).Message.MessageID
 
+				// Add the inline buttons to a reply.
 				keyboard := tgbotapi.InlineKeyboardMarkup{}
 				var row1 []tgbotapi.InlineKeyboardButton
 				var row2 []tgbotapi.InlineKeyboardButton
-
 				row1 = append(row1,
-					tgbotapi.NewInlineKeyboardButtonData("20 min", "min20"),
-					tgbotapi.NewInlineKeyboardButtonData("1 hour", "hour1"),
-					tgbotapi.NewInlineKeyboardButtonData("3 hours", "hour1"),
-					tgbotapi.NewInlineKeyboardButtonData("1 day", "day1"),
+					tgbotapi.NewInlineKeyboardButtonData("20 min", "20 min"),
+					tgbotapi.NewInlineKeyboardButtonData("1 hour", "1 hour"),
+					tgbotapi.NewInlineKeyboardButtonData("3 hours", "3 hours"),
+					tgbotapi.NewInlineKeyboardButtonData("1 day", "1 day"),
+				)
+				row2 = append(row2,
+					tgbotapi.NewInlineKeyboardButtonData("\u2705 Mark as Complete", "Complete"),
 				)
 
-				row2 = append(row2,
-					tgbotapi.NewInlineKeyboardButtonData("\u2705 Mark as Complete", "Complete"))
-				keyboard.InlineKeyboard = append(keyboard.InlineKeyboard, row1, row2)
-
-				msg.ReplyMarkup = keyboard
+				// One more button if the debug mode is on.
+				if bot.Debug {
+					row2 = append(row2,
+						tgbotapi.NewInlineKeyboardButtonData("5 sec", "sec5"),
+					)
+				}
 
 				(*reminder).IsSent = true
+
+				keyboard.InlineKeyboard = append(keyboard.InlineKeyboard, row1, row2)
+				msg.ReplyMarkup = keyboard
 				bot.Send(msg)
 
 			}
 		}
 		time.Sleep(1 * time.Second)
 	}
-
 }
 
+// NewConfig creates a new Config.
 func NewConfig() Config {
 	token := os.Getenv("TOKEN")
 	duration := 20
@@ -150,19 +182,22 @@ func main() {
 
 	remindersList := []*Reminder{}
 
-	go ReminderProcessor(&remindersList, bot)
+	go ReminderHandler(&remindersList, bot)
 
 	for update := range updates {
 
+		// Handle a callback
 		if update.CallbackQuery != nil {
-			CallbackProcessor(*update.CallbackQuery, &remindersList, bot)
+			CallbackHandler(*update.CallbackQuery, &remindersList, bot)
 			continue
 		}
 
+		// Do nothing if the message text is empty or callback received
 		if update.Message == nil {
 			continue
 		}
 
+		// Show the welcome message
 		if update.Message.Text == "/start" {
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Hi! Just write me or forward a message and I'll remind you about it in 20 minutes. \u23F0")
 			bot.Send(msg)
@@ -177,8 +212,7 @@ func main() {
 			IsSent:     false,
 		}
 
+		// Put a reminder in the qeue
 		remindersList = append(remindersList, &reminder)
-
 	}
-
 }
